15e0860829cca8ed403297ac647ced91f154069f
algammal <algammal@eg.ibm.com>
2020-03-30 05:59:57 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Items from './components/Items'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Items/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Items.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Items extends Component{
+    render(){
+        return(
+            <div>
+              <input/>  
+              <button>X</button>
+              <button>Add</button>
+            </div>
+        )      
+    }
+}
+export default Items;
