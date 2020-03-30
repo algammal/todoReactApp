dca15992025318c7cd1e56a0ef032cf25ce1992c
algammal <algammal@eg.ibm.com>
2020-03-30 20:06:33 +0200

    fixed and created list component issues

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Items from './components/AddItem'
+import List from './components/List'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-        <Items/>
-    </div>
+class App extends Component{
+  state ={
+    items:[]
+}
+addItem = (item) =>{
+    let items = this.state.items;
+    items.push(item);
+    this.setState({items})
+    
+   }
+  render(){
+    return (
+      <div className="App">
+
+        <Items addItem={this.addItem}/>
+        <List items={this.state.items}/>
+      </div>
   );
+}
+  
 }
 
 export default App;
